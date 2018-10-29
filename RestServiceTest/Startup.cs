baddd31d54c6622390c04c2856cc75192427b0d4
mart966b <31879834+mart966b@users.noreply.github.com>
2018-10-29 16:38:30 +0100

    Cors implement

--- RestServiceTest/Startup.cs
+++ RestServiceTest/Startup.cs
@@ -24,6 +24,7 @@ namespace RestServiceTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,7 +35,9 @@ namespace RestServiceTest
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(
+                options => { options.AllowAnyOrigin().AllowAnyMethod(); });
+            //allow everything from anywhere
             app.UseMvc();
         }
     }
